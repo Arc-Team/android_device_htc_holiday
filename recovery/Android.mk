88a0d97462ed587ca3c26967e7559b84be1360f9
JoseGalRe <josegalre@gmail.com>
2014-10-09 22:09:18 -0600

    some minor clean up and add flags to twrp

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE       := init.recovery.holiday.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.recovery.holiday.rc
-LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
