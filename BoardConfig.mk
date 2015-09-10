01f1d2436c05a253f3ae10ec8ffc57a600372efe
WildfireDEV <wildfiredev@gmail.com>
2015-09-10 11:08:29 +1000

    Set Metadata to false to allow for flashing from a older recovery

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -203,3 +203,6 @@ DEVICE_PACKAGE_OVERLAYS += device/htc/holiday/overlay
 
 # USB
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
+
+# Recovery settings for old recoveries
+USE_SET_METADATA := false
